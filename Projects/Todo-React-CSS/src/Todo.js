547a978df66f551a2616798d5d811ab04b037306
Daniel Awde <danielawde9@gmail.com>
2023-09-07 10:21:34 +0300

    updated

--- Projects/Todo-React-CSS/src/Todo.js
+++ Projects/Todo-React-CSS/src/Todo.js
@@ -1,22 +1,33 @@
 import "./styles.css";
-// function addition(a, b){ return a+b}
-const Todo = ({ todo, removeTodo, index, completeTodo }) => (
-  // if todo.isCompleted is true line thru else mashi
+
+// This React component represents an individual todo item.
+const Todo = ({
+  todo, // Object containing todo details (e.g., text and completion status).
+  removeTodo, // Function to remove the current todo.
+  index, // Index of the current todo in the list.
+  completeTodo, // Function to mark the current todo as completed.
+}) => (
+  // Display the todo item. If the todo is completed, its text will have a line-through style.
   <div
     className="todo-item"
     style={{ textDecoration: todo.isCompleted ? "line-through" : "none" }}
   >
+    {/* // Checkbox to mark the todo as completed. */}
     <input
       type="checkbox"
       onChange={() => {
+        // it gives the index to which item to remove from todos array
         completeTodo(index);
       }}
     />
+    {/* // Display the todo text from todo object. */}
     <span className="todo-text">{todo.todoText}</span>
+    {/* // Delete button to remove the todo. */}
     <div className="delete-action">
       <button
         className="delete-button"
         onClick={() => {
+          // it gives the index to which item to remove from todos array
           removeTodo(index);
         }}
       >
@@ -25,4 +36,5 @@ const Todo = ({ todo, removeTodo, index, completeTodo }) => (
     </div>
   </div>
 );
+
 export default Todo;
--- Projects/Todo-React-CSS/src/TodoList.js
+++ Projects/Todo-React-CSS/src/TodoList.js
@@ -9,6 +9,7 @@ const TodoList = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // if todo trim y3ne bala spaces fadye return ma tkafe be2e lcode
     if (!todo.trim()) return;
     // if (todo.trim()) {
     addTodosArray(todo);
@@ -21,6 +22,17 @@ const TodoList = () => {
   // im copyong orignal array w updating the new array
   // im replacing the old array with the new array
   const addTodosArray = (todoText) => {
+    // at first todos is empty, at the first todo added todos will be like
+    // todos [
+    //   {
+    //    todoText: "clean car",
+    //    isCompleted: false
+    //   },
+    //   {
+    //    todoText: "walk outside",
+    //    isCompleted: false
+    //   },
+    // ]
     const newTodos = [...todos, { todoText, isCompleted: false }];
     setTodos(newTodos);
   };
@@ -52,13 +64,41 @@ const TodoList = () => {
     const newTodos = [...todos];
     // it will get the current value of the isCompleted w bye2leba
     // if true bsir false , w eza false bsir double false equal true
+    // index  = 2
+    // bde ghayir isCompleted la 3 element bl todos array
+    // for example
+
+    //   { he the index = 2
+    //     todoText: "oiuhoiuh",
+    //     isCompleted: false
+    // },
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
   };
 
   const removeTodo = (index) => {
+    /*
+    todosArray 
+    [
+         {
+             todoText: "daniel",
+             isCompleted: true
+         },
+         {
+             todoText: "dasdasd",
+             isCompleted: false,
+         },
+         {
+             todoText: "oiuhoiuh",
+             isCompleted: false
+         },
+         .....
+     ]
+     */
     const newTodos = [...todos];
+    newTodos[index].isCompleted = false;
     newTodos.splice(index, 1);
+
     setTodos(newTodos);
   };
 
@@ -78,16 +118,17 @@ const TodoList = () => {
         <input type="submit" className="add-button" />
       </form>
       {/* if todos.length is not equal to zero show me the div className="paper" */}
+      {/* () && () */}
       {todos.length !== 0 && (
         <div className="paper" style={{ marginTop: "20px", padding: "20px" }}>
           {todos.map((todoItem, index) => {
             return (
               <Todo
-                todo={todoItem}
-                key={index}
                 completeTodo={completeTodo}
                 removeTodo={removeTodo}
+                key={index}
                 index={index}
+                todo={todoItem}
               />
             );
           })}
